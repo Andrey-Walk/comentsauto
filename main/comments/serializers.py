8e4eb880a19089a2401728c83f8c68be1365c42f
Walk <KsKam7@yandex.ru>
2023-07-02 10:07:27 +0300

    Save xlsx & csv books

--- main/comments/serializers.py
+++ main/comments/serializers.py
@@ -2,15 +2,21 @@ from rest_framework import serializers
 from .models import Countrys, Directors, Cars, Comments
 
 
+class CountrysSerializer(serializers.ModelSerializer):
+    directors = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Countrys
+        fields = ('country', 'directors')
+
+    def get_directors(self, obj):
+        directors = Directors.objects.filter(country=obj)
+        return DirectorsSerializer(directors, many=True).data
+
 class DirectorsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Directors
         fields = ('country', 'director')
-
-class CountrysSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Countrys
-        fields = ('country',)
 
 class CarsSerializer(serializers.ModelSerializer):
     class Meta:
